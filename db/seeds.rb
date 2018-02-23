0fb7fb9982a10da415ab64d24475db2e58b28de6
jayapanday <jaya@maropost.com>
2018-02-23 21:04:33 +0530

    categories in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.find_or_create_by(name: 'Clothing')
+Category.find_or_create_by(name: 'Footwears')
+Category.find_or_create_by(name: 'Indian Dresses')
+Category.find_or_create_by(name: 'Fancy')
+
+
+
