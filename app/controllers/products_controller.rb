76b110f8cf3e93ef8d264bb1c8f9e0a6ee196b64
jayapanday <jaya@maropost.com>
2018-02-23 20:54:13 +0530

    first commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,53 @@
+class ProductsController < ApplicationController
+	before_action :find_product, only:[:show, :edit, :update, :destroy]
+	before_action :authenticate_user!
+	def index
+		@products = Product.all.order("created_at DESC")
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.create(product_params)		
+		if @product.save
+			redirect_to root_path
+		else
+		render 'new' 
+		end
+
+	end
+
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	def edit
+	end
+
+	def update
+		if @product.update(product_params)
+			redirect_to root_path (@product)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@product.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name, :price, :status, :image)
+	end
+
+	def find_product
+		@product = Product.find(params[:id])
+	end
+
+
+end
